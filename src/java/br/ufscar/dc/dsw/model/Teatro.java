00cb7c941fa0179e4a9a73c200e98dd51125cc04
Carlos Eduardo Ferreira <duduferreira_01@hotmail.com>
2019-04-17 02:29:31 -0300

    DAO e model modificado

---
+++ src/java/br/ufscar/dc/dsw/model/Teatro.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufscar.dc.dsw.model;
+
+/**
+ *
+ * @author carlos
+ */
+public class Teatro {
+    private String email;
+    private String senha;
+    private String cnpj;
+    private String nome;
+    private String cidade;
+
+    public Teatro() {
+    }
+
+    public Teatro(String email, String senha, String cnpj, String nome, String cidade) {
+        this.email = email;
+        this.senha = senha;
+        this.cnpj = cnpj;
+        this.nome = nome;
+        this.cidade = cidade;
+    }
+    
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the cnpj
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    
+}

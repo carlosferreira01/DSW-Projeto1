28ac219c5b5127acea44c7913c5d66ed2ca261b6
Carlos Eduardo Ferreira <duduferreira_01@hotmail.com>
2019-04-17 02:32:09 -0300

    DAO e model modificado

---
+++ Projeto1/src/java/br/ufscar/dc/dsw/model/Vendas.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufscar.dc.dsw.model;
+
+/**
+ *
+ * @author carlos
+ */
+public class Vendas {
+    private String email;
+    private String senha;
+    private String url;
+    private String nome;
+    private String telefone;
+    
+    public Vendas(){
+    }
+    
+    public Vendas(String email, String senha, String url, String nome, String telefone) {
+        this.email = email;
+        this.url = url;
+        this.senha = senha;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
